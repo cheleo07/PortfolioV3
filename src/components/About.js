6a2169ba00c6a59aea5f0814f5db42f1c201ba88
Leo <l.chevalier@it-mation.com>
2021-09-28 11:13:49 +0200

    cv

--- src/components/About.js
+++ src/components/About.js
@@ -14,7 +14,7 @@ export default function About() {
                     </p>
                     <div className="flex justify-center">
                         <a
-                            href="CV.pdf"
+                            href="CV (4).pdf"
                             className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
                             TELECHARGER CV
                         </a>
